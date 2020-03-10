9699466a071f3a12dd01a6ad9697c031c034c33c
HasSan-68 <48790045+HasSan-68@users.noreply.github.com>
2020-03-10 12:30:48 +0100

    Index

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\OptredenRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,9 +11,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(OptredenRepository $optredenRepository)
     {
         return $this->render('home/index.html.twig', [
+            'optredens' => $optredenRepository->findAll(),
             'controller_name' => 'HomeController',
         ]);
     }
